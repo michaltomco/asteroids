f93c0c7ffcfeba0d6dd9f4f480edc55338388edc
Michal Tomčo <tomco.mito@gmail.com>
2026-05-08 10:28:27 +0200

    Initialize Asteroids pygame project

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import constants
+
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print("Screen width: 1280")
+    print("Screen height: 720")
+if __name__ == "__main__":
+    main()
+    
+    
+    
+    
+def funct():
+	print("format")
