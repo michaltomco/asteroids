4cf7f93f909032ad0c0b62e387b536af39643b2a
Michal Tomčo <tomco.mito@gmail.com>
2026-05-08 13:16:28 +0200

    Log game state during main loop

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame
+
 import constants
+from logger import log_state
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print("Screen width: 1280")
     print("Screen height: 720")
+
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 if __name__ == "__main__":
     main()
-    
-    
-    
-    
-def funct():
-	print("format")
