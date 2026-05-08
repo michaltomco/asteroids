3b58b48bf5597d6a27418b9a9c20ec96abf4413d
Michal Tomčo <tomco.mito@gmail.com>
2026-05-08 14:13:46 +0200

    Add player triangle with rotation controls

--- main.py
+++ main.py
@@ -2,23 +2,36 @@ import pygame
 
 import constants
 from logger import log_state
+from player import Player
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print("Screen width: 1280")
-    print("Screen height: 720")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
-        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+
         screen.fill("black")
+        player.draw(screen)
+
+        log_state()
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
